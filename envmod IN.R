441c25f3bbc318b70136183152da797fe8cc04ef
lesteryuan <lyuan@yahoo.com>
2023-12-22 13:26:04 -0500

    revised models

--- envmod IN.R
+++ envmod IN.R
@@ -11,10 +11,11 @@ envmod_IN <- function(envdata, pred, var = "cond") {
     df1 <- envdata
 
     ## drop forest...correlated with human activity
-    varlist <- varlist[-which(varlist == "pctdecid2019ws")]
-    varlist <- varlist[-which(varlist == "pctmxfst2019ws")]
-    varlist <- varlist[-which(varlist=="pctsilicicws")]
-    print(nrow(df1))
+#    varlist <- varlist[-which(varlist == "pctdecid2019ws")]
+#    varlist <- varlist[-which(varlist == "pctmxfst2019ws")]
+#    varlist <- varlist[-which(varlist=="pctsilicicws")]
+    ## drop kffact for now because of incomplete coverage
+    varlist <- varlist[-which(varlist=="kffactws")]
 
 
     if (var == "cond") {
@@ -22,7 +23,12 @@ envmod_IN <- function(envdata, pred, var = "cond") {
         ## drop 6 outliers
         incvec <- df1$cond > 3.5
         cond.mn <- tapply(df1$cond, df1$STATION_NAME, mean)
-        
+        cond.sd <- tapply(df1$cond, df1$STATION_NAME, sd)
+
+        nsamp <- tapply(df1$cond, df1$STATION_NAME, function(x) sum(!is.na(x)))
+        incvec <- nsamp > 0
+        cond.mn <- cond.mn[incvec]
+
         df2 <- data.frame(STATION_NAME = names(cond.mn),
                           cond = as.vector(cond.mn))
     }
@@ -51,66 +57,110 @@ envmod_IN <- function(envdata, pred, var = "cond") {
     }
     pred <- unique.data.frame(pred[, c("LSite", varlist)])
 
-    ## cluster predictor variables
-    dftemp <- na.omit(pred[, varlist])
-    cor0 <- 1-abs(cor(dftemp[, varlist], method = "spearman"))
+    docluster <- F
+    if (docluster) {
+        ## cluster predictor variables
+        dftemp <- na.omit(pred[, varlist])
+        cor0 <- 1-abs(cor(dftemp[, varlist], method = "spearman"))
 
-    require(cluster)
-    clust0 <- agnes(cor0, diss = T, method = "average")
+        require(cluster)
+        clust0 <- agnes(cor0, diss = T, method = "average")
 
-    lab0 <- varlist
-    dev.new()
-    par(mar = c(2,1,1,4), mgp = c(2.3,1,0))
-    plot(clust0,labels = lab0,  which.plots = 2,
+        lab0 <- varlist
+        dev.new()
+        par(mar = c(2,1,1,4), mgp = c(2.3,1,0))
+        plot(clust0,labels = lab0,  which.plots = 2,
          main = "", ylab = "" , xlab = "", sub = "", axes
-         = F)
-    axis(4, at = seq(0, 1, by = 0.2), lab = seq(1, 0, by = -0.2))
-    mtext(expression(abs(italic(r[s]))),side = 4, line = 2.3)
+             = F)
+        axis(4, at = seq(0, 1, by = 0.2), lab = seq(1, 0, by = -0.2))
+        mtext(expression(abs(italic(r[s]))),side = 4, line = 2.3)
+    }
 
     print(varlist)
     print(nrow(df2))
     df1 <- merge(df2, pred, by.y = "LSite", by.x = "STATION_NAME")
     print(nrow(df1))
-        
-    ## drop missing kfact
-    incvec <- !is.na(df1$kffactws)
-    df1 <- df1[incvec,]
-    print(summary(df1))
 
+    ## drop missing kfact
+#    incvec <- !is.na(df1$kffactws)
+#    df1 <- df1[incvec,]
+#    print(summary(df1))
+
+    ## latitude stratification
+    plot(df1$LATITUDE_MEASURE, df1$rckdepws, xlab = "Latitude",
+         ylab = "Bedrock depth", pch = 21, col = "grey39", bg = "white")
+    abline(v = 39.6)
+    incvec <- df1$LATITUDE_MEASURE < 39.6
+
+    dflist <- split(df1, incvec)
+    yall <- c(dflist[[1]][, var], dflist[[2]][, var])
+    predlist <- as.list(rep(NA, times = length(dflist)))
+    modlist <- as.list(rep(NA, times = length(dflist)))
+
+    for (i in 1:length(dflist)) {
+        modlist[[i]] <- ranger(data = dflist[[i]][, c(var, varlist)],
+                      dependent.variable.name = var,
+                      num.trees = 5000, importance = "permutation")
+
+        predlist[[i]] <- modlist[[i]]$predictions
+    }
+    predall <- c(predlist[[1]], predlist[[2]])
+
+    dev.new()
+    par(mar = c(4,4,1,1), mgp = c(2.3,1,0), bty = "l")
+    plot(predall, yall, pch = 21, col = "grey", bg = "white", axes = F,
+         xlab = "Predicted", ylab ="Observed")
+    logtick.exp(0.001, 10, c(1,2), c(T,T))
+    abline(0,1)
+    print(mean((predall - yall)^2))
+
+#    vimport <- mod$variable.importance
+#    vimport <- sort(vimport)
+#    print(vimport)
+    ## data frame with human activities set to zero
+    varadj <- c("pctimp2019ws", "pcturbop2019ws", "pctcrop2019ws")
+    predref <- as.list(rep(NA,times=2))
+    for (i in 1:length(dflist)) {
+        new.data2 <- dflist[[i]]
+        for (j in varadj) new.data2[,j] <- 0
+        ## plot "ref" vs. observed difference
+        predref[[i]] <- predict(modlist[[i]], new.data2)$predictions
+    }
+    predref.all <- c(predref[[1]], predref[[2]])
+
+    require(maps)
+    require(mapproj)
+    dev.new()
+    par(mar = c(1,1,1,1))
+    map("state", region = "indiana", proj = "albers", par = c(30,40))
+    delt <- exp(predall- predref.all) - 1
+    cex0 <- delt
+    cex0[cex0 < 0] <- 0
+    cex0 <- cex0*4 + 0.1
+
+    pout <- mapproject(c(dflist[[1]]$LONGITUDE_MEASURE,
+                         dflist[[2]]$LONGITUDE_MEASURE),
+                       c(dflist[[1]]$LATITUDE_MEASURE,
+                         dflist[[2]]$LATITUDE_MEASURE), proj = "")
+    points(pout$x, pout$y, pch = 21, col = "grey39", bg = "white",
+           cex = cex0)
+    stop()
+
+    ## plot partial dependence relationship
     ## set up dataframe for calculating partial dependence relationship
     new.data <- data.frame(matrix(NA, ncol = length(varlist), nrow = 40))
     names(new.data) <- varlist
     for (i in varlist) {
         new.data[, i] <- median(df1[, i])
     }
-#    new.data$pctcrop2019ws <- seq(min(df1$pctcrop2019ws),
-#                                  max(df1$pctcrop2019ws), length = 40)
-
-    new.data$wetindexws <- seq(min(df1$wetindexws),
-                               max(df1$wetindexws), length = 40)
-    new.data2 <- df1
-    new.data2$pctcrop2019ws <- 0
-
-    mod <- ranger(data = df1[, c(var, varlist)],
-                  dependent.variable.name = var,
-                  num.trees = 5000, importance = "permutation")
-    print(mod)
-    plot(mod$predictions, df1[, var])
-
-    vimport <- mod$variable.importance
-    vimport <- sort(vimport)
-    print(vimport)
+    varpick <- "pctcrop2019ws"
+    new.data[, varpick]<- seq(min(df1[,varpick]),
+                                  max(df1[,varpick]), length = 40)
 
     dev.new()
     predout <- predict(mod, new.data)
-    plot(new.data$wetindexws, predout$predictions)
-    stop()
-    predout <- predict(mod, new.data2)
+    plot(new.data[, varpick], predout$predictions, xlab = varpick)
 
-    dev.new()
-    plot(mod$predictions, predout$predictions - mod$predictions, col = "grey")
-    abline(h = 0)
-    
 
 
 }
@@ -221,11 +271,11 @@ envmod <- function(dfenv, idname) {
     set.seed(10)
     envtest <- dfenv
     envref <- dfenv
-    ## set human land use variables to a low percentile 
+    ## set human land use variables to a low percentile
     ## of all sites in the new data set
     selref <- envref$rt.nrsa == "R"
     humpct <- c("pctcrop2019ws", "pctimp2019ws", "pcthay2019ws", "npdesdensws",
-                "pcturbop2019ws", "pctag2006slp10ws", 
+                "pcturbop2019ws", "pctag2006slp10ws",
                 "pctnonagintrodmanagvegws")
 
     pvals <- 0.25
